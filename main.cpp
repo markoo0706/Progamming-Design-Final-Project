6bc64943aebd61416904aaef73bf15148feca149
AndyFanChen <99866172+AndyFanChen@users.noreply.github.com>
2022-12-16 23:04:55 +0800

    csvLoad

--- main.cpp
+++ main.cpp
@@ -2,19 +2,25 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <string>
 using namespace std;
 
-void loadPrice(char* fileName, double** priceInfo);
+void loadPrice(const string& fileName, string** priceInfo, const int colNum);
 void loadEco();
 
 int main(){
 
-    char fileName[30] = "SP500_date.csv";
-    double** priceInfo;
-    loadPrice(fileName, priceInfo);
+    string fileName = R"(C:\Users\Andy Chen\Documents\GitHub\Progamming-Design-Final-Project\SP500_date.csv)";
+    const int rowNum = 7900;
+    const int colNum = 5;
+    string** priceInfo = new string*[rowNum]; // 7900為最大列數
+    for(int i = 0; i < rowNum; i++){
+        priceInfo[i] = new string[colNum];
+    }
+    loadPrice(fileName, priceInfo, colNum);
     return 0;
 }
-void loadPrice(char* fileName, double** priceInfo){ // 需要dateNum
+void loadPrice(const string& fileName, string** priceInfo, const int colNum){ // 需要dateNum
     ifstream ifs;							//创建流对象
 
     ifs.open(fileName, ios::in);	//打开文件
@@ -24,7 +30,6 @@ void loadPrice(char* fileName, double** priceInfo){ // 需要dateNum
         cout << "openWrong";
         return;
     }
-
     vector<string> item;		//用于存放文件中的一行数据
 
     string temp;				//临时存储文件中的一行数据
@@ -33,7 +38,7 @@ void loadPrice(char* fileName, double** priceInfo){ // 需要dateNum
     {
         item.push_back(temp);
     }
-
+    int rowCount = 0;
     //遍历文件中的每一行数据
     for (auto it = item.begin(); it != item.end(); it++)
     {
@@ -42,25 +47,15 @@ void loadPrice(char* fileName, double** priceInfo){ // 需要dateNum
         //其作用是把字符串分解为单词(在此处就是把一行数据分为单个数据)
         istringstream istr(*it);
 
-        //将字符串流 istr 中的字符读入到 str 字符串中，读取方式是以逗号为分隔符
-        getline(istr, str, ',');
-        cout << str << "\t";            // str 对应第一列数据
-        //atoi(str.c_str())该函数将字符转化为 int 数据
-
-        getline(istr, str, ',');
-        cout << str << "\t";            // str 对应第二列数据
-
-        getline(istr, str, ',');
-        cout << str << "\t";            // str 对应第三列数据
-
-        getline(istr, str, ',');
-        cout << str << "\t";            // str 对应第四列数据
-
-        getline(istr, str, ',');
-        cout << str << "\t";            // str 对应第五列数据
-
-        getline(istr, str, ',');
-        cout << str << endl;            // str 对应第六列数据
+        // 一列一列輸入，第0個item是日期
+        for (int i = 0; i < colNum; i++){
+            getline(istr, str, ',');
+            priceInfo[rowCount][i] = str;
+//            if(rowCount == 0) cout << str;
+            if (i < colNum - 1) cout << priceInfo[rowCount++][i] << '\t';
+            else cout << priceInfo[rowCount++][i] << endl;
+        }
+        rowCount++;
     }
 
 }
